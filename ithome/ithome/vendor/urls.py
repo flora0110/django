49a1d03c01b8bf9873db4ca4e8a801e62ea05aaa
flora110 <flora90.1.10@gmail.com>
2022-04-18 00:17:04 +0800

    day21

--- ithome/ithome/vendor/urls.py
+++ ithome/ithome/vendor/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.showtemplate),
+    path('create', views.vendor_create_view),
+    path('<int:id>/', views.singleVendor, name='vendor'),
 ]
--- ithome/ithome/vendor/views.py
+++ ithome/ithome/vendor/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404  # 新增
+from django.http import Http404
+from .forms import VendorForm, RawVendorForm  # 要記得 import 相對應的 Model Form 唷!
 from django.shortcuts import render
 from .models import Vendor
 # Create your views here.
@@ -6,4 +9,33 @@ from .models import Vendor
 def showtemplate(request):
     vendor_list = Vendor.objects.all()  # 把所有 Vendor 的資料取出來
     context = {'vendor_list': vendor_list}  # 建立 Dict對應到Vendor的資料，
-    return render(request, 'vendor\detail2.html', context)
+    return render(request, 'vendors\detail2.html', context)
+
+
+# 針對 vendor_create.html
+
+
+def vendor_create_view(request):
+    form = RawVendorForm(request.POST or None)
+    print(form)
+    print(form.cleaned_data)
+    if form.is_valid():
+        Vendor.objects.create(**form.cleaned_data)  # 新增
+        form = RawVendorForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "vendors/vendor_create.html", context)
+
+
+def singleVendor(request, id):
+    vendor_list = get_object_or_404(Vendor, id=id)
+    # try:
+    #     vendor_list = Vendor.objects.get(id=id)
+    # except Vendor.DoesNotExist:
+    #     raise Http404
+    context = {
+        'vendor_list': vendor_list
+    }
+    return render(request, 'vendors/vendor_detail.html', context)
